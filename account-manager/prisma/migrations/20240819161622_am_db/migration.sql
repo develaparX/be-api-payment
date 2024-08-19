9ff49c7c1882326c3a356273c2d08d06e89a9219
arunikax <srarfian@gmail.com>
2024-08-20 06:05:58 +0700

    Remove Console Log

--- account-manager/prisma/migrations/20240819161622_am_db/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "TransactionHistory" ALTER COLUMN "toAddress" DROP NOT NULL;
--- payment-manager/src/controllers/recurringPaymentController.js
+++ payment-manager/src/controllers/recurringPaymentController.js
@@ -1,9 +1,8 @@
 const recurringPaymentService = require('../services/recurringPaymentService');
 
 async function createRecurringPayment(req, res) {
-  const userId = req.user.id; // Diperoleh dari middleware
+  const userId = req.user.id; 
   const { accountId, amount, currency, interval, toAccountId } = req.body;
-  console.log(req.body)
   try {
     const recurringPayment = await recurringPaymentService.createRecurringPayment({
       accountId,
@@ -13,18 +12,17 @@ async function createRecurringPayment(req, res) {
       interval,
       userId,
       active: true,
-      nextPaymentDate: new Date() // Mulai dari hari ini
+      nextPaymentDate: new Date() 
     });
     res.status(201).send(recurringPayment);
   } catch (error) {
-      console.error('Error creating recurring payment:', error); // Menampilkan detail error
   res.status(500).send({ error: 'Failed to create recurring payment', details: error.message });
 
   }
 }
 
 async function getAllRecurringPayments(req, res) {
-  const userId = req.userId; // Diperoleh dari middleware
+  const userId = req.userId; 
 
   try {
     const payments = await recurringPaymentService.getAllRecurringPaymentsByUserId(userId);
